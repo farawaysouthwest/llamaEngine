61fcf043d0451a74c44764d9d63b4fb61cc03c5c
Mitch Alderson <mitchell@apollographql.com>
2022-05-25 10:12:26 -0700

    remove js files from ts compile

--- llama/src/resolvers/query.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getLlamas(parent, args, context) {
-    // check auth
-    return context.dataSources.llamas.fetchLlamas();
-}
-exports.default = {
-    llamas: getLlamas,
-};
